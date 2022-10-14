3c4d01c046dfb73f8a12fdf1a9b38aa0b9f78d55
Megan Massa <megan.massa@umces.edu>
2022-10-14 15:30:51 -0400

    Ch1 to EBC, GOF progress

--- scripts/analysis/goodness_of_fit_testing.R
+++ scripts/analysis/goodness_of_fit_testing.R
@@ -5,7 +5,7 @@
 
 library(tidyverse)
 library(unmarked)
-
+source('scripts/wrangling/tidy_method_unmarked.R')
 
 # Data import -------------------------------------------------------------
 
@@ -21,8 +21,6 @@ read_rds(paste0('output/all_fitted_models_', sp, '.rds')) %>%
 read_rds('data/processed/umf.rds') %>% 
   list2env(.GlobalEnv)
 
-umf <- umf_GRSP
-
 
  # Goodness of fit testing -------------------------------------------------
 
@@ -33,17 +31,43 @@ umf <- umf_GRSP
 
 # Create an array to hold coefficient estimates
 simrep <- 10
-estimates <-
-  array(NA, dim = c(length(coef(m.0)), simrep))
-rownames(estimates) <- names(coef(m.0))
 nyears <- 7 
 
-site_sample <-
-  sample(1:242, 242, replace = TRUE)
+# Create an array to hold coefficient estimates
+estimates <-
+  array(NA, dim = c(length(coef(best_full)), simrep))
 
-y_sample <- NULL
-obsCovs_sample <- NULL
+rownames(estimates) <- names(coef(best_full))
+
+estimates
+
+site_sample <-
+  sample(unique(detections$grts), 242, replace = TRUE) %>% sort()
+
+## Detection sample ----
+y_sample <- 
+  bind_cols(grts = site_sample) %>% left_join(detections) %>% 
+  filter(species == sp)
+
+nrow(y_sample) == nrow(detections %>% filter(species == sp))
+
+## Obs cov sample ----
+combos <- expand.grid(site_sample, c(1:6, 8))
+
+obsCovs_sample <- 
+  map(
+    1:length(obs_covs),
+    function(x) sprintf('%s_%s', combos[,1], combos[,2]) %>% as_tibble_col(column_name = "grts_year") %>% left_join(pluck(obs_covs, x) %>% rownames_to_column(var = "grts_year")))
+
+# NEED TO FIX SORT
+
+names(obsCovs_sample) <- names(obs_covs)
+
+## Site cov sample ----
+  
 siteCovs_sample <- NULL
+
+
 
 for(t in 1:nyears) {
   # detections for year T of subsample
