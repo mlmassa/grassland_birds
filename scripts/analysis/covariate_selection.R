3cf6e8d5bcbf4d69e00aab4e4697e3504bd86dbd
Megan Massa <megan.massa@umces.edu>
2023-02-13 23:55:11 -0500

    Finished chapter 1

--- scripts/analysis/covariate_selection.R
+++ scripts/analysis/covariate_selection.R
@@ -25,6 +25,7 @@ static_covs %>%
     cols = c(2:length(.)),
     names_to = 'variable') %>% 
   left_join(static_covs %>% select(grts, park)) %>% 
+  filter(!str_detect(variable, "_5000")) %>% 
 
   
 ggplot(
@@ -54,7 +55,7 @@ ggplot(
 ggsave(
   filename = 'cov_hists_park.png',
     path = 'output/plots',
-    width = 6,
+    width = 5,
     height = 8,
     units = 'in')
 
@@ -73,7 +74,8 @@ get_lower_tri <-
 corr_table <- 
   static_covs %>% 
   select(order(colnames(static_covs))) %>% 
-  select(where(is.numeric)) %>% 
+  select(where(is.numeric)) %>%
+  select(!contains("_5000")) %>% 
   select(-c(grts, ever_burned, harvest_limit, leased)) %>% 
   
   # Run correlation
@@ -83,8 +85,9 @@ corr_table <-
   round(digits = 2) %>% 
   
   # Get only one triangle
-  get_lower_tri() %>% 
+  # get_lower_tri() %>% 
   as_tibble(rownames = 'var1') %>% 
+
   
   # Transform to long tibble with var1, var2, value
   pivot_longer(
@@ -96,13 +99,57 @@ corr_table <-
 # Show most highly correlated variables
 corr_table %>% 
   filter(value != 1,
-         abs(value) >= 0.6) %>% 
+         abs(value) >= 0.5) %>% 
   arrange(-abs(value)) %>% 
   print(n = nrow(.))
 
 # Basic triangle plot
 plot <-
   corr_table %>% 
+  mutate(
+    var1 = 
+      factor(
+        var1,
+        levels = c(
+                   "grs_250", "grs_500", "grs_1000", "grs_2500",
+                   "for_250", "for_500", "for_1000", "for_2500",
+                   "dvp_250", "dvp_500", "dvp_1000", "dvp_2500",
+                   "crp_250", "crp_500", "crp_1000", "crp_2500",
+                   "shrub",
+                   "trees",
+                   "angle_max",
+                   "angle_mean"),
+        labels = c(
+                   "Grassland (250 m)", "Grassland (500 m)", "Grassland (1 km)", "Grassland (2.5 km)",
+                   "Forest (250 m)", "Forest (500 m)", "Forest (1 km)", "Forest (2.5 km)",
+                   "Developed (250 m)", "Developed (500 m)", "Developed (1 km)", "Developed (2.5 km)",
+                   "Cropland (250 m)", "Cropland (500m)", "Cropland (1 km)", "Cropland (2.5 km)",
+                   "Shrub",
+                   "Trees",
+                   "Max. angle",
+                   "Mean angle")),
+    var2 = 
+      factor(
+        var2,
+        levels = c(
+                   "grs_250", "grs_500", "grs_1000", "grs_2500",
+                   "for_250", "for_500", "for_1000", "for_2500",
+                   "dvp_250", "dvp_500", "dvp_1000", "dvp_2500",
+                   "crp_250", "crp_500", "crp_1000", "crp_2500",
+                   "shrub",
+                   "trees",
+                   "angle_max",
+                   "angle_mean"),
+        labels = c(
+                   "Grassland (250 m)", "Grassland (500 m)", "Grassland (1 km)", "Grassland (2.5 km)",
+                   "Forest (250 m)", "Forest (500 m)", "Forest (1 km)", "Forest (2.5 km)",
+                   "Developed (250 m)", "Developed (500 m)", "Developed (1 km)", "Developed (2.5 km)",
+                   "Cropland (250 m)", "Cropland (500m)", "Cropland (1 km)", "Cropland (2.5 km)",
+                   "Shrub",
+                   "Trees",
+                   "Max. angle",
+                   "Mean angle"))
+  ) %>% 
   ggplot(
     aes(x = var1, y = var2, fill = value)) +
   geom_tile(
@@ -114,9 +161,9 @@ plot <-
     midpoint = 0,
     limit = c(-1,1),
     space = 'Lab',
-    name = 'Spearman\ncorrelation') +
-  scale_y_discrete(position = "right") +
+    name = 'Spearman correlation') +
   theme_minimal() +
+  coord_flip() +
   theme(
     axis.text.x = 
       element_text(angle = 90, vjust = 0.5, hjust = 1, size = 11),
@@ -129,7 +176,7 @@ plot <-
 labeled_corr_plot <- 
   plot +  
     geom_text(
-      aes(var1, var2, label = value), color = 'black', size = 2) +
+      aes(var1, var2, label = value), color = 'black', size = 1.75) +
     theme(
       axis.title.x = element_blank(),
       axis.title.y = element_blank(),
@@ -137,13 +184,13 @@ labeled_corr_plot <-
       panel.border = element_blank(),
       panel.background = element_blank(),
       axis.ticks = element_blank(),
-      legend.justification = c(1, 0),
-      legend.position = c(0.6, 0.7),
+      legend.justification = c(0.5, 0),
+      legend.position = "top",
       legend.direction = "horizontal") +
     guides(
       fill = 
         guide_colorbar(
-          barwidth = 7, 
+          barwidth = 8, 
           barheight = 1,
           title.position = "top", 
           title.hjust = 0.5))
@@ -154,8 +201,8 @@ labeled_corr_plot
 ggsave(
   filename = 'corr_matrix_update.png',
     path = 'output/plots',
-    width = 7.5,
-    height = 5,
+    width = 6.5,
+    height = 6,
     units = 'in')
 
 # Random other plots ------------------------------------------------------
